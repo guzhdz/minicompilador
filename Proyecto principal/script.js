7f2bcd2c289cddb741c3b2ed760f14c3fce10b84
guzhdz <89165084+guzhdz@users.noreply.github.com>
2023-01-29 15:13:22 -0600

    Actualizacion visual Analizador Lexico

--- Proyecto principal/script.js
+++ Proyecto principal/script.js
@@ -12,20 +12,81 @@ const imprimirRubros = (simbolo, tipoS, tipo) => {
     console.log(simbolo + tipoS + tipo);
 }
 
-let fuente = "-{}while , casa*int 39.1 120";
-let an = new Analizador(fuente);
+const generarFilas = (simbolo, tipoS, tipo) => {
+    let row = document.createElement("DIV");
 
-console.log("Resultado del analisis Lexico");
-if(fuente.length != 0) {
-    console.log("Simbolo             Tipo           Id_tipo   ");
+    let column1 = document.createElement("DIV");
+    let span1 = document.createElement("SPAN");
+    column1.classList.add("column");
+    span1.textContent = simbolo;
+    column1.appendChild(span1);
+    row.appendChild(column1);
 
-    an.obtenerSimbolo();
+    let column2 = document.createElement("DIV");
+    let span2 = document.createElement("SPAN");
+    column2.classList.add("column");
+    span2.textContent = tipoS;
+    column2.appendChild(span2);
+    row.appendChild(column2);
 
-    while(an.simbolo != "%%") {
-        imprimirRubros(an.simbolo, an.tipoS, an.tipo);
-        an.obtenerSimbolo();
-    }
-} else {
-    console.log("Entrada vacia");
+    let column3 = document.createElement("DIV");
+    let span3 = document.createElement("SPAN");
+    column3.classList.add("column");
+    span3.textContent = tipo;
+    column3.appendChild(span3);
+    row.appendChild(column3);
+
+    row.classList.add("row", "row-content");
+
+    return row;
 }
 
+let fuente = "";
+const botonAnLexico = document.getElementById("boton-lexico");
+botonAnLexico.addEventListener("click", () => {
+    const input = document.getElementById("input-lexico");
+    fuente = input.value;
+
+    let an = new Analizador(fuente);
+
+    console.log("Resultado del analisis Lexico");
+
+    if(fuente.length != 0) {
+        console.log("Simbolo             Tipo           Id_tipo   ");
+        an.obtenerSimbolo();
+        
+        let resultado = document.getElementById("resultado-lexico");
+        let tabla = resultado.firstElementChild;
+        let body = document.getElementById("body");
+        body.removeChild(body.lastElementChild);
+        resultado.classList.remove("resultado");
+
+        [...tabla.children].forEach(element => {
+            if(element != tabla.firstElementChild) {
+                tabla.removeChild(element);
+            } 
+        });
+
+        while(an.simbolo != "%%") {
+            imprimirRubros(an.simbolo, an.tipoS, an.tipo);
+            
+            tabla.appendChild(generarFilas(an.simbolo, an.tipoS, an.tipo));
+
+            an.obtenerSimbolo();
+        }
+    } else {
+        console.log("Entrada vacia");
+        let resultado = document.getElementById("resultado-lexico");
+        resultado.classList.add("resultado");
+
+        let body = document.getElementById("body");
+        let vacio = document.createElement("DIV");
+        let span = document.createElement("SPAN");
+
+        span.textContent = "Entrada vacia";
+        vacio.appendChild(span);
+        vacio.classList.add("vacio");
+        body.appendChild(vacio);
+
+    }
+});
